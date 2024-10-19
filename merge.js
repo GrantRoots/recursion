0e7cf57d038392b9ab2022fd7e99c974d00c2541
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-10-18 19:31:02 -0700

    Fix and remove comments
    Removed comments and console.logs and also fixed the code where it wouldnt sort if they were equal.

--- merge.js
+++ merge.js
@@ -1,34 +1,13 @@
 function mergeSort(array) {
-    //if = 1 then reuturn
     if (array.length < 2) {
         return array
-        // then compare this number to the other sorted half
     }
     //split array
     let newArrayLeft = array.slice(0, Math.round(array.length / 2))
-    console.log(newArrayLeft, 'newarrayleft')
     let newArrayRight = array.slice(Math.round(array.length / 2), array.length)
-    console.log(newArrayRight, 'newarrayright')
 
-    //both at once??
     let newLeft = mergeSort(newArrayLeft)
-    console.log(newLeft, "newleft")
-    let newRight = mergeSort(newArrayRight) //newRight = half right half of new left
-    console.log(newRight, 'newRight')
-
-    // mergeSort([2, 1, 0, 3])
-    //     mergeSort([2, 1,]) returns [1, 2]
-    //         mergeSort([2]) //returns newLeft = 2
-    //         mergeSort([1]) // returns newRight = 1
-
-    //     mergeSort([0, 3]) Why doesnt this run??
-    //         mergeSort([0]) // returns newLeft = 0
-    //         mergeSort([3]) // returns newRight = 3
-        
-
-
-    //dont use .sort and compare value by value
-    // let sortedArray = array.sort((newLeft, newRight) => newLeft - newRight)
+    let newRight = mergeSort(newArrayRight)
 
     let sortedArray = []
 
@@ -41,15 +20,15 @@ function mergeSort(array) {
             sortedArray.push(newRight[0])
             newRight.shift()
         }
+        if (newLeft[0] === newRight[0] && (newLeft.length !== 0 && newRight.length !== 0)) {
+            sortedArray.push(newLeft[0])
+            newLeft.shift()
+        }
     }
 
-    console.log(sortedArray, 'sortedArray')
     return sortedArray
-
-    //array = [2, 1, 0, 3]
 }
-console.log(mergeSort([2, 1, 0, 3]))
 
-// console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
-// console.log('next')
-// console.log(mergeSort([105, 79, 100, 110]))
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
+console.log('next')
+console.log(mergeSort([105, 79, 100, 110]))
